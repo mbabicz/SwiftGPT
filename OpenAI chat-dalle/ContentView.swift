68963aa82da9effe930e9d92efd79a1331127f5b
kz <michal@babicz.dev>
2023-02-07 10:52:00 +0100

    Initial Commit

---
+++ OpenAI chat-dalle/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  OpenAI chat-dalle
+//
+//  Created by kz on 07/02/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ OpenAI chat-dalle/OpenAI_chat_dalleApp.swift
@@ -0,0 +1,17 @@
+//
+//  OpenAI_chat_dalleApp.swift
+//  OpenAI chat-dalle
+//
+//  Created by kz on 07/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct OpenAI_chat_dalleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
